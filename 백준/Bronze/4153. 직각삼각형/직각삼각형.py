8f5c69f25ca5ebc40cddb938a397f8c1720349bb
Kang myeongseo <128151078+imkmss@users.noreply.github.com>
2025-09-19 12:27:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,14 @@
+
+while True:
+    a,b,c=map(int, input().split())
+
+    if a==0 and b==0 and c==0:
+        break
+    nums=sorted([a,b,c])
+
+    if(nums[0]**2+nums[1]**2==nums[2]**2):
+        print("right")
+    else:
+        print("wrong")
+
+
