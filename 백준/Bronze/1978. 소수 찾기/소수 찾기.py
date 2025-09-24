588db11e17669c5c7f8c3d72533a3d9d9327894c
Kang myeongseo <128151078+imkmss@users.noreply.github.com>
2025-09-24 11:35:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+import math
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2, int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+
+N=int(input())
+nums=list(map(int,input().split()))
+
+count=0
+for num in nums:
+    if is_prime(num):
+        count+=1
+print(count)
